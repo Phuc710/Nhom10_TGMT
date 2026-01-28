cdb2515d6857011681d03b98c256d2ee03561ac8
Phuc710 <athanhphuc7102005@gmail.com>
2026-01-28 16:16:05 +0700

    day02_continue

--- day02/xulyimg.py
+++ day02/xulyimg.py
@@ -14,15 +14,51 @@ def add_noise(img):
     noisy_img = cv.add(img, gauss)
     return noisy_img
 
+def add_muoi_tieu(img, ratio=0.02):
+    noisy_img = img.copy()
+    soluong = (ratio * img.size)
+    toado = [np.random.randint(0, i - 1, int(soluong)) for i in img.shape]
+    noisy_img[toado[0], toado[1]] = 255
+    toado = [np.random.randint(0, i - 1, int(soluong)) for i in img.shape]
+    noisy_img[toado[0], toado[1]] = 0
+    return noisy_img
+
+def draw_zone(img):
+    h, w = img.shape[:2]
+    overlay = img.copy()
+
+    pts = np.array([
+        [int(0.1*w), h],
+        [int(0.45*w), int(0.6*h)],
+        [int(0.55*w), int(0.6*h)],
+        [int(0.9*w), h]
+    ], np.int32)
+
+    cv.polylines(overlay, [pts], True, (0, 255, 0), 3)
+    return overlay
+
+
 if __name__ == "__main__":
-    url = "https://raw.githubusercontent.com/opencv/opencv/refs/heads/4.x/samples/data/lena.jpg"
+    url = "https://raw.githubusercontent.com/udacity/CarND-LaneLines-P1/master/test_images/solidWhiteCurve.jpg"
     img = read_img_url(url)
+    img = cv.resize(img, (960, 540))
+    
 
-    cv.imshow("Image 1", img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    # cv.imshow("Image 1", img)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
 
-    noisy_img = add_noise(img)
-    cv.imshow("Noisy Image", noisy_img)
+    # noisy_img = add_noise(img)
+    # cv.imshow("Noisy Image", noisy_img)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
+
+    # img3 = add_muoi_tieu(img, ratio=0.05)
+    # cv.imshow("Muoi Tieu Image", img3)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+    zone_img = draw_zone(img)
+    cv.imshow("Zone", zone_img)
     cv.waitKey(0)
     cv.destroyAllWindows()
